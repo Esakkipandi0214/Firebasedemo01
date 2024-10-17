de7548a188924e243efd726a1aede73f175b9380
Esakkipandi0214 <praveenpandian01@gmail.com>
2024-10-17 13:07:24 +0530

    update

---
+++ src/pages/Cards/index.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import FoodDetails from '@/Components/cards/foodDetails'
+const index = () => {
+  return (
+    <div className=' p-1 h-screen'>
+        <FoodDetails/>
+    </div>
+  )
+}
+
+export default index
